c02483117f7fa36ab9daeb80649c628eaab40b3c
Alexander Gridin <alexander.m.gridin@gmail.com>
2022-04-02 16:28:01 +0300

    page layout updated

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'rc-header',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Input() public pageTitle!: string;
+
   public isShowSidenav = false;
   public readonly appName = 'Read Companion';
 
